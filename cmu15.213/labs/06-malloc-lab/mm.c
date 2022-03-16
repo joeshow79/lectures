6cd8b195996b954702f45658be56debb0867bdce
jasonj <35540346@qq.com>
2022-03-16 17:11:49 +0800

    add coalesce && mm_check.

--- cmu15.213/labs/06-malloc-lab/mm.c
+++ cmu15.213/labs/06-malloc-lab/mm.c
@@ -43,8 +43,6 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-// FIXME: since the allocation is ALIGNMENT bytes aligned
-// the granularity of the allocation is ALIGNMENT bytes?
 #define SEGLIST_MIN_LEVEL 3
 // maximum capacity is 16M since maximum heap is MAX_HEAP(20M)
 #define SEGLIST_MAX_LEVEL 24
@@ -54,8 +52,11 @@ team_t team = {
 
 //  get the class level of the seglist from which try to allocate mem
 #define GET_ALLOCATION_FROM_CLASS(size) (log2n_of_power2(roundup_to_power2(size)) - SEGLIST_START_LEVEL)
+
 // get the class level of the seglist to which try to put the free mem record
-#define GET_ALLOCATION_CLASS(size) (GET_ALLOCATION_FROM_CLASS(size + 1) - 1)
+// NOTE: the largest class host all the blocks above the limit size
+#define GET_ALLOCATION_CLASS(size) ((GET_ALLOCATION_FROM_CLASS(size + 1) - 1) >= SEGLIST_CLASSES ? \
+                                        SEGLIST_CLASSES: (GET_ALLOCATION_FROM_CLASS(size + 1) - 1))
 
 #define ASSERT_CLASS(cls) { \
     assert(cls >= (SEGLIST_MIN_LEVEL - SEGLIST_START_LEVEL)); \
@@ -72,11 +73,15 @@ typedef BASETYPE *  PTRTYPE;
 #define VALUE_OF_PTR(ptr) (*((PTRTYPE)ptr))
 #define RAW_PTR_TO_SLPTR(ptr) ((PTRTYPE)ptr + 1)
 
-#define HEADER_PTR(ptr) ((PTRTYPE)ptr - 1)
-#define FOOTER_PTR(ptr, size) ((PTRTYPE)((char*)((PTRTYPE)ptr - 1) + size))
 #define BLOCK_PTR(ptr) ptr
+#define HEADER_PTR(ptr) ((PTRTYPE)ptr - 1)
+#define FOOTER_PTR(ptr, size) (PTRTYPE)((char*)ptr + size)
 #define BLOCK_NEXT_PTR(ptr) ptr
 #define BLOCK_PREV_PTR(ptr) ((PTRTYPE)ptr + 1)
+
+#define NEXT(ptr) ((PTRTYPE)VALUE_OF_PTR(BLOCK_NEXT_PTR(ptr)))
+#define PREV(ptr) ((PTRTYPE)VALUE_OF_PTR(BLOCK_PREV_PTR(ptr)))
+
 #define SET_BLOCK_NEXT_PTR(ptr, p) (VALUE_OF_PTR(BLOCK_NEXT_PTR(ptr)) = (BASETYPE)p)
 #define SET_BLOCK_PREV_PTR(ptr, p) (VALUE_OF_PTR(BLOCK_PREV_PTR(ptr)) = (BASETYPE)p)
 
@@ -86,20 +91,26 @@ typedef BASETYPE *  PTRTYPE;
     VALUE_OF_PTR(HEADER_PTR(ptr)) |= 0x1;    // Allocated flag
 #define SET_FOOTER_ALLOC(ptr, size)  VALUE_OF_PTR(FOOTER_PTR(ptr, size)) = size; \
     VALUE_OF_PTR(FOOTER_PTR(ptr, size)) |= 0x1;    // Allocated flag
-#define SET_HEADER_VALID(ptr, size)  VALUE_OF_PTR(HEADER_PTR(ptr)) = size;
-#define SET_FOOTER_VALID(ptr, size)  VALUE_OF_PTR(FOOTER_PTR(ptr, size)) = size;
-// 0 for valid, 1 for allocated
-#define RESET_HEADER_VALID(ptr, size) VALUE_OF_PTR(HEADER_PTR(ptr)) &= (-2);    // clear allocated flag
-#define RESET_FOOTER_VALID(ptr, size) VALUE_OF_PTR(FOOTER_PTR(ptr, size)) &= (-2);
+
+#define SET_HEADER_VALID(ptr, size)  VALUE_OF_PTR(HEADER_PTR(ptr)) = size
+#define SET_FOOTER_VALID(ptr, size)  VALUE_OF_PTR(FOOTER_PTR(ptr, size)) = size
+
+// 0 for valid for allocation
+// 1 for already allocated
+#define RESET_HEADER_VALID(ptr, size) VALUE_OF_PTR(HEADER_PTR(ptr)) &= (-2)    // clear allocated flag
+#define RESET_FOOTER_VALID(ptr, size) VALUE_OF_PTR(FOOTER_PTR(ptr, size)) &= (-2)
+
+#define IS_BLOCK_VALID(ptr) ((VALUE_OF_PTR(HEADER_PTR(ptr)) & (0x1)) == 0)
 
 #define SPLIT_BLOCK_PTR(ptr, size) ((PTRTYPE)((char*)((PTRTYPE)ptr + 2) + size))
 #define SPLIT_BLOCK_PAYLOAD_SIZE(orig, alloc) (orig - alloc - HEADER_SIZE - FOOTER_SIZE)
 
 #define BLOCK_OVERHEAD_SIZE (HEADER_SIZE + FOOTER_SIZE)
 #define BLOCK_PAYLOAD_SIZE(size) (size - HEADER_SIZE - FOOTER_SIZE)
-#define BLOCK_TOTAL_SIZE(size) (size + HEADER_SIZE + FOOTER_SIZE)
-#define BLOCK_MINIMUM_SIZE (HEADER_SIZE + FOOTER_SIZE + MIN_PAYLOAD_SIZE)
+#define BLOCK_TOTAL_SIZE(size) (size + BLOCK_OVERHEAD_SIZE)
+#define BLOCK_MINIMUM_SIZE (MIN_PAYLOAD_SIZE + BLOCK_OVERHEAD_SIZE)
 
+#define ENABLE_COALESCE 1
 #define ENABLE_LOG 0
 
 #if ENABLE_LOG
@@ -108,6 +119,8 @@ typedef BASETYPE *  PTRTYPE;
 #define LOG(...)
 #endif
 
+#define ERR(...) printf(__VA_ARGS__)
+
 int roundup_to_power2(size_t n) {
     --n;
     n |= n >> 1;
@@ -115,7 +128,7 @@ int roundup_to_power2(size_t n) {
     n |= n >> 4;
     n |= n >> 8;
     n |= n >> 16;
-    // needed for 64bit data
+    // needed for 64bit data, not for 32bit
     /*n |= n >> 32;*/
     ++n;
 
@@ -156,13 +169,13 @@ int log2n_of_power2(size_t v) {
 /*}*/
 
 /**
- * seglist
+ * seglist -- segregated free lists
  */
 typedef struct seglist_t {
-    /*roots[0]  free block with size range in: 2^SEGLIST_MIN_LEVEL ~ 2^(SEGLIST_MIN_LEVEL+1)-1*/
-    /*roots[1]  2^(SEGLIST_MIN_LEVEL+1) ~ 2^(SEGLIST_MIN_LEVEL+2)-1*/
+    /*roots[0]  free block with size range: [2^SEGLIST_MIN_LEVEL ~ 2^(SEGLIST_MIN_LEVEL+1)-1]*/
+    /*roots[1]  [2^(SEGLIST_MIN_LEVEL+1) ~ 2^(SEGLIST_MIN_LEVEL+2)-1]*/
     /*...*/
-    /*roots[-1]  2^(SEGLIST_MAX_LEVEL)*/
+    /*roots[-1]  [2^(SEGLIST_MAX_LEVEL), infi/upper]*/
 
     /**
      *BLOCK
@@ -197,10 +210,10 @@ void seglist_dump(char* msg, struct seglist_t* sl) {
             LOG("\nSEGLIST[%d]:\t", i);
 
         while (NULL != p) {
-            LOG("[(addr) 0x%x][(sz) %d]\t((next) 0x%x : (prev) 0x%x) -> \n", (BASETYPE)(p), BLOCK_SIZE_HEADER(p),
-                    VALUE_OF_PTR(BLOCK_NEXT_PTR(p)), VALUE_OF_PTR(BLOCK_PREV_PTR(p)));
+            LOG("[(addr) 0x%x][(sz) %d]\t((next) 0x%x : (prev) 0x%x) -> \n", (BASETYPE)(p),
+                    BLOCK_SIZE_HEADER(p), (BASETYPE)NEXT(p), (BASETYPE)PREV(p));
 
-            p = (PTRTYPE)VALUE_OF_PTR(BLOCK_NEXT_PTR(p));
+            p = NEXT(p);
         }
     }
 }
@@ -211,11 +224,12 @@ void seglist_dump(char* msg, struct seglist_t* sl) {
  * fptr: ptr to free block
  * size: size of free block, must be the size of ALIGNMENT bytes aligned memory block
  */
-void seglist_add_item(struct seglist_t* seglist, PTRTYPE new_block_ptr, size_t size){
+void seglist_add_item(struct seglist_t* seglist, PTRTYPE new_block_ptr, size_t size) {
     assert(size <= (1 << SEGLIST_MAX_LEVEL));
     assert(size >= ALIGNMENT);
 
     // NOTE: Another policy that not used
+    // the idea is split the block to multi classes
     // Reallocate the free memory into seperate classes list
     // Example;
     //   Remainder  |  Half=(Remainder+8)/2  |  Residual=Remainder-Half
@@ -223,25 +237,6 @@ void seglist_add_item(struct seglist_t* seglist, PTRTYPE new_block_ptr, size_t s
     //    56        |             32         |          24
     //    24        |             16         |          8
     //    8         |             8          |          0
-
-    /*size_t remainder = size;*/
-    /*void* ptr = fptr;*/
-    /*while(remainder) {*/
-        /*size_t half = (remainder + ALIGNMENT) / 2;*/
-        /*size_t residual = remainder - residual;*/
-        /*size_t cls = log2n(half) - SEGLIST_MIN_LEVEL;*/
-
-        /*ASSERT_CLASS(cls)*/
-
-        /*// TODO*/
-        /*void* cptr = seglist->roots[cls];*/
-
-        /*ptr = ptr + half;*/
-        /*remainder = residual;*/
-    /*}*/
-
-    /** (new_block_ptr - 1)  = size;*/
-    /*printf("Header ptr:%x\n", HEADER_PTR(new_block_ptr));*/
 
     SET_HEADER_VALID(new_block_ptr, size);
     SET_FOOTER_VALID(new_block_ptr, size);
@@ -256,6 +251,7 @@ void seglist_add_item(struct seglist_t* seglist, PTRTYPE new_block_ptr, size_t s
     PTRTYPE tptr = seglist->roots[to_cls];
     if(NULL != tptr) {
         LOG("seglist not empty, try to relink\n");
+
         // new_block->next = old_block
         SET_BLOCK_NEXT_PTR(new_block_ptr, tptr);
         // old_block->prev = new_block
@@ -275,8 +271,6 @@ void* seglist_try_malloc(struct seglist_t* seglist, size_t size){
 
     LOG("seglist_try_malloc from class %d for %d->%d bytes mem\n", cls, size, asize);
 
-    /*seglist_dump(seglist);*/
-
     ASSERT_CLASS(cls);
 
     for(int i = cls; i < SEGLIST_CLASSES; ++i) {
@@ -287,11 +281,11 @@ void* seglist_try_malloc(struct seglist_t* seglist, size_t size){
 
             if(block_size < asize) {
                 // ptr = ptr->next
-                ptr = (PTRTYPE)VALUE_OF_PTR(BLOCK_NEXT_PTR(ptr));
+                ptr = NEXT(ptr);
                 continue;
             }
 
-            PTRTYPE pnext = (PTRTYPE)(VALUE_OF_PTR(BLOCK_NEXT_PTR(ptr)));
+            PTRTYPE pnext =  NEXT(ptr);
 
             // remove the block from the list since will allocate the mem from the head
             // TODO: verify pnext is valid pointer
@@ -318,8 +312,6 @@ void* seglist_try_malloc(struct seglist_t* seglist, size_t size){
                SET_HEADER_ALLOC(ptr, asize);
                SET_FOOTER_ALLOC(ptr, asize);
 
-               // TODO: remove old block
-
                // Add split block back to seglist
                seglist_add_item(seglist, new_block_ptr, new_block_sz);
                seglist_dump("after split", seglist);
@@ -333,7 +325,190 @@ void* seglist_try_malloc(struct seglist_t* seglist, size_t size){
     return NULL;
 }
 
+/**
+ * Return:
+ * 0: Blocks not adjecent
+ * 1: blocks pointted by ptr_a and ptr_b are adjecent and block A preceed block B
+ * -1: blocks pointted by ptr_a and ptr_b are adjecent and block B preceed block A
+ */
+int is_block_adjecent(PTRTYPE ptr_a, PTRTYPE ptr_b) {
+    assert(NULL != ptr_a);
+    assert(NULL != ptr_b);
+
+    PTRTYPE header_ptr_a = HEADER_PTR(ptr_a);
+    PTRTYPE header_ptr_b = HEADER_PTR(ptr_b);
+    size_t sz_a = BLOCK_SIZE_HEADER(ptr_a);
+    PTRTYPE footer_ptr_a = FOOTER_PTR(ptr_a, sz_a);
+    size_t sz_b = BLOCK_SIZE_HEADER(ptr_b);
+    PTRTYPE footer_ptr_b = FOOTER_PTR(ptr_b, sz_b);
+
+    BASETYPE header_a = (BASETYPE) header_ptr_a;
+    BASETYPE header_b = (BASETYPE) header_ptr_b;
+    BASETYPE footer_a = (BASETYPE) footer_ptr_a;
+    BASETYPE footer_b = (BASETYPE) footer_ptr_b;
+
+#define IS_ADJECENT(a, b) ((a-4) == b)
+    /*LOG("check is_adjecent 0x%x:%d and 0x%x:%d\n", (BASETYPE)ptr_a, sz_a, (BASETYPE)ptr_b, sz_b);*/
+    /*LOG("a header: 0x%x, b header: 0x%x\n", (BASETYPE)header_a, (BASETYPE)header_b);*/
+    /*LOG("a footer: 0x%x, b footer: 0x%x\n", (BASETYPE)footer_a, (BASETYPE)footer_b);*/
+    if (IS_ADJECENT(header_a, footer_b)) {
+        return -1;
+    }
+
+    if (IS_ADJECENT(header_b, footer_a)) {
+        return 1;
+    }
+#undef IS_ADJECENT
+
+    return 0;
+}
+
+/**
+ * merge the 2 blocks
+ * Note: front is preceed read
+ * Before merge
+     *Block front
+     *  [  size_f   ]
+     *  [   next    ]
+     *  [   prev    ]
+     *  [           ]
+     *  [ blk front ]
+     *  [           ]
+     *  [  size_f   ]
+     *
+     *Block rear
+     *  [  size_r   ]
+     *  [   next    ]
+     *  [   prev    ]
+     *  [           ]
+     *  [ blk front ]
+     *  [           ]
+     *  [  size_r   ]
+     *
+ * After merge
+     *Block
+     *  [ size_f+r  ]
+     *  [   next    ]
+     *  [   prev    ]
+     *  [           ]
+     *  [ blk front ]
+     *  [           ]
+     *  [ size_f+r  ]
+ */
+PTRTYPE merge_block(PTRTYPE front, PTRTYPE rear) {
+    assert(1 == is_block_adjecent(front, rear));
+
+    size_t sz_f = BLOCK_SIZE_HEADER(front);
+    size_t sz_r = BLOCK_SIZE_HEADER(rear);
+    size_t sz_n = sz_f + sz_r + BLOCK_OVERHEAD_SIZE;   // 1 header and 1 footer eliminated
+
+    LOG("merge_block 0x%x:%d and 0x%x:%d to 0x%x:%d\n",
+            (BASETYPE)front, sz_f, (BASETYPE)rear, sz_r, (BASETYPE)front, sz_n);
+
+    SET_HEADER_VALID(front, sz_n);
+    SET_FOOTER_VALID(front, sz_n);
+
+    return front;
+}
+
+
+/*
+ * Reclaim the block into seglist, and do coalescing before that
+ */
+void seglist_reclaim(struct seglist_t* seglist, PTRTYPE ptr) {
+    for(int i = 0; i < SEGLIST_CLASSES; ++i) {
+        PTRTYPE iter = seglist->roots[i];
+
+        while(NULL != iter) {
+            int ret = is_block_adjecent(iter, ptr);
+
+            PTRTYPE inext = NEXT(iter);
+
+            // iter precede ptr
+            if (0 != ret) {
+                PTRTYPE iprev = PREV(iter);
+
+                // iter is head
+                if (seglist->roots[i] == iter) {
+                    seglist->roots[i] = inext;
+
+                } else {
+                    if (NULL != iprev) {
+                        // iter->prev->next = iter->next
+                        SET_BLOCK_NEXT_PTR(iprev, inext);
+                    }
+                }
+
+                if (NULL != inext) {
+                    // iter->next->prev = iter->prev
+                    SET_BLOCK_PREV_PTR(inext, iprev);
+                }
+
+                PTRTYPE new_block;
+
+                if (1 == ret) {
+                    new_block = merge_block(iter, ptr);
+                }
+
+                if (-1 == ret) {
+                    new_block = merge_block(ptr, iter);
+                }
+
+                // recursive(at most 2 passes) to make sure all adjecent blocks been coalesced
+                // this could solve the case that the reclaimed block adjecent
+                // to one preceding block and one posterior block
+                //
+                // Before reclaim blockR (which adjecent blockA and blockB both)
+                // [blockA] ... [blockBsymotion-prefix)] (blockA and blockB are not adjecent)
+                //
+                // Pass 1:
+                // [blockA+R][blockB]
+                //
+                // Pass 2:
+                // [blockA+R+B]
+                return seglist_reclaim(seglist, new_block);
+            }
+
+            // iter = iter->next
+            iter = inext;
+        }
+    }
+
+    size_t sz = BLOCK_SIZE_HEADER(ptr);
+    seglist_add_item(seglist, ptr, sz);
+}
+
 seglist_t* seglist_g;
+
+/*
+ *
+ * mm_check - seglist sanity checking
+ */
+int mm_check(struct seglist_t* seglist) {
+    for(int i = 0; i < SEGLIST_CLASSES; ++i) {
+        PTRTYPE iter = seglist->roots[i];
+
+        while(NULL != iter) {
+            // 1:  block marked as empty?
+            if (! IS_BLOCK_VALID(iter)) {
+                ERR("block sanity checking failed addr:0x%x sz:%d valid:%d\n",
+                        (BASETYPE)iter, BLOCK_SIZE_HEADER(iter), IS_BLOCK_VALID(iter));
+
+                assert(0);
+            }
+
+            // 2: any continous free blocks escaped coalescing?
+
+            // 3: pointer in the free list point to valid free blocks?
+
+            // 4: any overlapping block
+
+            iter = NEXT(iter);
+        }
+    }
+
+    return 0;
+}
 
 /*
  * mm_init - initialize the malloc package.
@@ -345,7 +520,7 @@ int mm_init(void)
 
     seglist_init(seglist_g);
 
-    // shift  4 byte for 8 byte alignment requirement
+    // NOTE: shift  4 byte for 8 byte alignment requirement
     // the 4 bytes are leaked from seglist point of view
     mem_sbrk(4);
 
@@ -384,7 +559,6 @@ void *mm_malloc(size_t size)
         seglist_dump("after mem_sbrk", seglist_g);
 
         // try second allocation from seglist managed blocks
-        // TODO: need to rethink if size is correct
         ret = seglist_try_malloc(seglist_g, BLOCK_PAYLOAD_SIZE(newsize));
 
         assert(NULL != ret);
@@ -408,8 +582,15 @@ void mm_free(void *ptr)
     SET_BLOCK_NEXT_PTR(ptr, 0);
     SET_BLOCK_PREV_PTR(ptr, 0);
 
+#ifdef ENABLE_COALESCE
+    seglist_reclaim(seglist_g, ptr);
+#else
     seglist_add_item(seglist_g, ptr, sz);
-    seglist_dump("after mm_free mem", seglist_g);
+#endif
+
+    seglist_dump("after mm_free", seglist_g);
+
+    mm_check(seglist_g);
 }
 
 /*
@@ -451,17 +632,3 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 #endif
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
